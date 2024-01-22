471199fa54efc55049e45992e560036b2faeb24a
YashAryan <Yashsiwach07@gmail.com>
2024-01-22 20:56:22 +0530

    New Files

---
+++ Python/Datatype.py
@@ -0,0 +1,17 @@
+a= 1
+b = "Yash"
+c = 2.0
+d = ["This" , " is ", "a", "list"]
+e = ("This ", "is ","a ", "tuple")
+f = True
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+d[2] = 22
+e[1] = 22
+print(d)
+print(e)
+
